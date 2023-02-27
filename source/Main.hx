a105a1745d1a4d1e8771ae6b18c2c840bdf8335a
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-02-27 06:41:27 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -71,7 +71,7 @@ class Main extends Sprite
 
 		Paths.getModFolders();
 		ClientPrefs.startControls();
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, #if (flixel < "5.0.0")  framerate, framerate, skipSplash, startFullscreen));
 
 		#if !mobile
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
